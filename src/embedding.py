b12223b0f27e062b64c0a6b3ba8626e3664469ae
MinShun1 <minshun159@gmail.com>
2026-07-26 21:19:45 +0700

    Update embedding.py
    perubahan skema cara menarik API KEY

--- src/embedding.py
+++ src/embedding.py
@@ -1,11 +1,13 @@
 from google import genai
 
-client = genai.Client(
-    api_key="YOUR_API_KEY"
-)
+client = None
+
+def initialize_client(api_key):
+    global client
+    client = genai.Client(api_key=api_key)
+
 
 def embed_text(text):
-
     response = client.models.embed_content(
         model="gemini-embedding-001",
         contents=text
